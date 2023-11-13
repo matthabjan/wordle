a0cf6cb3540efd108861a19a9d7894db2795c1b0
matthabjan <99269377+matthabjan@users.noreply.github.com>
2023-11-13 22:45:41 +0100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { GAME_TITLE } from './constants/strings';
+import { vi } from '@jest/globals';
+
+// Mock the ResizeObserver
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+// Stub the global ResizeObserver
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);
 
 beforeEach(() => {
+  // Mock matchMedia
   Object.defineProperty(window, 'matchMedia', {
     writable: true,
     value: jest.fn().mockImplementation((query) => ({
       matches: false,
       media: query,
       onchange: null,
-      addListener: jest.fn(), // deprecated
-      removeListener: jest.fn(), // deprecated
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
       addEventListener: jest.fn(),
       removeEventListener: jest.fn(),
       dispatchEvent: jest.fn(),
